ab83c3903cd096dfeec5b636ecee1f3591044f74
rtwohey <rtwohey@ionic.io>
2024-04-10 15:28:11 -0400

    Updated private route to check for the session with getSnapshot from the external store

--- demos/security-trifecta/src/core/PrivateRoute.tsx
+++ demos/security-trifecta/src/core/PrivateRoute.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from 'react';
 import { Redirect, Route, useLocation } from 'react-router';
 import { useAuth } from './AuthProvider';
+import { getSnapshot } from '../utils/session-vault';
 
 type Props = { children?: ReactNode };
 
 export const PrivateRoute = ({ children }: Props) => {
-  const { session } = useAuth();
+  const session = getSnapshot();
 
   // If there is no session, redirect the user to the login page.
   if (!session) return <Redirect to="/" />;
