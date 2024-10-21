307d50fec1b2d4b8863b98c83ca359a6625cbc51
Ken Sodemann <ken.sodemann@gmail.com>
2024-10-21 18:03:34 -0500

    test out the tutorial

---
+++ identity-vault/getting-started/src/pages/Login.tsx
@@ -0,0 +1,16 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+const Login = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent></IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
---
+++ identity-vault/getting-started/src/pages/Unlock.tsx
@@ -0,0 +1,16 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+const Unlock = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Unlock</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent></IonContent>
+    </IonPage>
+  );
+};
+
+export default Unlock;
